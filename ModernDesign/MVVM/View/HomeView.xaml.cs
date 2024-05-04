02561fe474f513024bdc17aa660681adae55e9c5
nic00la <nic00la.kaleta@gmail.com>
2024-05-04 18:38:25 +0200

    Text & StackPanel ✅

---
+++ ModernDesign/MVVM/View/HomeView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace ModernDesign.MVVM.View;
+
+public partial class HomeView : UserControl
+{
+    public HomeView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ ModernDesign/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,11 @@
+using ModernDesign.Core;
+
+namespace ModernDesign.MVVM.ViewModel;
+
+public class MainViewModel : ObservableObject
+{
+    public MainViewModel()
+    {
+        
+    }
+}
